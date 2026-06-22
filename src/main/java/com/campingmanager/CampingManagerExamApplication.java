acafe0abf9ad17acf5c6833804dcf3c17a9a5058
Michael <skyfallez4316@gmail.com>
2026-06-22 12:01:59 +0200

    Inizializzazione del progetto Spring Boot del camping manager

---
+++ src/main/java/com/campingmanager/CampingManagerExamApplication.java
@@ -0,0 +1,13 @@
+package com.campingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingManagerExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampingManagerExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/campingmanager/CampingManagerExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingManagerExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
